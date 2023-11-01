56bc4e100b76c4e40cea51b2fc502b81f2284e37
nazmul6548 <nazmul6548@gmail.com>
2023-11-01 16:28:33 +0600

    1st ans

---
+++ index.js
@@ -0,0 +1,6 @@
+// ১. একটা কোড লিখো। যেটা দিয়ে কোন একটা array এর মধ্যে সবচেয়ে ছোট সংখ্যা বের করে দিতে পারবে
+// _____ans____
+var randomNum =[2,3,4,7,59,98,53,87,-3];
+const result=Math.min(...randomNum)
+console.log(result);
+
