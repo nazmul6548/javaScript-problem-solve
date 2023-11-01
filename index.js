04c809c2ea67911666ec0ace173ddd26d609a0ed
nazmul6548 <nazmul6548@gmail.com>
2023-11-01 23:06:40 +0600

    13th ans

--- index.js
+++ index.js
@@ -91,3 +91,18 @@ var book2 = 4;
 var book3 = 6;
 var results = paperRequirements(book1, book2, book3);
 console.log(results);
+
+// ১৩. এইটা একটু ট্রিকি হতে পারে। একটা array এর মধ্যে অনেকগুলা সংখ্যা থাকবে। তোমার কাজ হচ্ছে সংখ্যা গুলা একটার পর একটা করে চেক করা। এবং সংখ্যা গুলা যদি পজিটিভ সংখ্যা হয়। অর্থাৎ শূন্য বা শূন্যের চাইতে বড় হয় তাহলে সেগুলাকে কোন একটা array এর মধ্যে রাখবে। আর যদি নেগেটিভ সংখ্যা হয়। তাহলে লুপটা স্টপ করে দিবে। এবং লুপ বন্ধ করার আগ পর্যন্ত যতগুলা পজিটিভ সংখ্যা পাওয়া গেছে। সেগুলা রিটার্ন করে দিবে।
+// _____ans____//
+function pNnumber(numbers) {
+  const positiveNum = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] >= 0) {
+      positiveNum.push(numbers[i]);
+    }
+  }
+  return positiveNum;
+}
+const input = [1, 2, 4, 5, 66, -3, -6, -8, 65];
+var returnNum = pNnumber(input);
+console.log(returnNum);
