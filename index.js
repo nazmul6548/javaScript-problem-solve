39a2eb03b37c61e1d87899cc0257cfc4b1442580
nazmul6548 <nazmul6548@gmail.com>
2023-11-01 18:03:59 +0600

    2nd ans

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ var randomNum =[2,3,4,7,59,98,53,87,-3];
 const result=Math.min(...randomNum)
 console.log(result);
 
+
+// ২. একটা কোড লিখো যেটা দিয়ে তিনটা সংখ্যার মধ্যে সবচেয়ে ছোট সংখ্যা বের করে দিবে।
+// _____ans____
+var num1 = -101;
+var num2 = 12;
+var num3 = 33;
+var results = num1;
+if (results < num2) {
+  console.log(num1);
+} else if (results < num3) {
+  console.log(num1);
+} else {
+  console.log("unknown");
+}
