bc0a1b501144d2e8fca952f0d9fdd86a36324d53
nazmul6548 <nazmul6548@gmail.com>
2023-11-01 18:12:38 +0600

    4th ans

--- index.js
+++ index.js
@@ -18,3 +18,18 @@ if (results < num2) {
 } else {
   console.log("unknown");
 }
+
+
+// ৪. একটা ফাংশন লিখো। সেটার মধ্যে তিনটা প্যারামিটার নিবে। এই তিনটা প্যারামিটার হবে কোন একটা ত্রিভুজের তিনটা বাহু এর দৈর্য্য। এখন তোমার কাজ হচ্ছে ফাংশনের ভিতরে কিছু হিসাব নিকাশ করে ত্রিভুজের area (আয়তন) বের করা। কোন একটা ত্রিভুজের তিনটা বাহুর দৈর্য্য দেয়া থাকলে সেটা থেকে কিভাবে আয়তন বের করতে হয় সেই ফর্মুলা গুগল থেকে খুঁজে বের করো।
+
+function triangle(a, b, c) {
+    // we are know that//
+    var s = (a + b + c) / 2;
+    var area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    return area;
+  }
+  var a = 13;
+  var b = 14;
+  var c = 15;
+  var areaResult = triangle(a, b, c);
+  console.log(areaResult);
