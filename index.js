fd60c08de4c5adbacf59ca12d850f533e93333f8
nazmul6548 <nazmul6548@gmail.com>
2023-11-01 20:08:43 +0600

    6th ans

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // ১. একটা কোড লিখো। যেটা দিয়ে কোন একটা array এর মধ্যে সবচেয়ে ছোট সংখ্যা বের করে দিতে পারবে
 // _____ans____
-var randomNum =[2,3,4,7,59,98,53,87,-3];
-const result=Math.min(...randomNum)
+var randomNum = [2, 3, 4, 7, 59, 98, 53, 87, -3];
+const result = Math.min(...randomNum);
 console.log(result);
-
 
 // ২. একটা কোড লিখো যেটা দিয়ে তিনটা সংখ্যার মধ্যে সবচেয়ে ছোট সংখ্যা বের করে দিবে।
 // _____ans____
@@ -19,17 +18,26 @@ if (results < num2) {
   console.log("unknown");
 }
 
-
 // ৪. একটা ফাংশন লিখো। সেটার মধ্যে তিনটা প্যারামিটার নিবে। এই তিনটা প্যারামিটার হবে কোন একটা ত্রিভুজের তিনটা বাহু এর দৈর্য্য। এখন তোমার কাজ হচ্ছে ফাংশনের ভিতরে কিছু হিসাব নিকাশ করে ত্রিভুজের area (আয়তন) বের করা। কোন একটা ত্রিভুজের তিনটা বাহুর দৈর্য্য দেয়া থাকলে সেটা থেকে কিভাবে আয়তন বের করতে হয় সেই ফর্মুলা গুগল থেকে খুঁজে বের করো।
 
 function triangle(a, b, c) {
-    // we are know that//
-    var s = (a + b + c) / 2;
-    var area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
-    return area;
-  }
-  var a = 13;
-  var b = 14;
-  var c = 15;
-  var areaResult = triangle(a, b, c);
-  console.log(areaResult);
+  // we are know that//
+  var s = (a + b + c) / 2;
+  var area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+  return area;
+}
+var a = 13;
+var b = 14;
+var c = 15;
+var areaResult = triangle(a, b, c);
+console.log(areaResult);
+
+//   ৬. তোমাকে যদি বলে একটা while লুপ দিয়ে ৭ থেকে ১৯ পর্যন্ত যতগুলা বিজোড় সংখ্যা আছে সেগুলা দেখাতে। তুমি কি সেটা দেখাতে পারবে? পারলে সেই কোড লিখে ফেলো।
+// _____ans____
+let x = 7;
+while (x <= 17) {
+  if (x % 2 !== 0 && x>=7) {
+    console.log(x)
+}
+  x++;
+}
